39a06beb78669ff96edb9bebbab806dd5a2cebb1
arao1209 <aneri.ami12@gmail.com>
2022-09-04 17:52:01 -0700

    added one variable

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -7,9 +7,14 @@ import java.util.List;
 
 public class Constants {
 
+    private Constants(){
+
+    }
+
     public static final String DIRECTORY_PATH = "C:\\Users\\Aneri\\OneDrive - Arizona State University\\Desktop\\Derex Griffin - 540\\reaboutgradingassignments";
     public static final List<String> validExtension = List.of("txt");
 
-    
+    public static final List<String> validExtension1 = List.of("txt");
+
 
 }
